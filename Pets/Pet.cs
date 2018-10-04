11ccf697ec7fe630b0e1c939fb9956134f0b4436
Jacob Ross <jross1818@gmail.com>
2018-10-03 19:54:53 -0500

    created pet class but not working

---
+++ Pets/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pets
+{
+    public class Pet
+    {
+        public string type;
+        public string name;
+        public string owner;
+        public double weight;
+
+        public Pet(string type, string name, string owner, double weight)
+        {
+            this.type = type;
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+        }
+        public string getTag()
+        {
+            return "If lost, call" + this.owner;
+        }
+    }
+}
